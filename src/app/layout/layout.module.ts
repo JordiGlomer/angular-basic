4d909cb8d0c942a8270ad6507f3a356971ccafaa
Alberto Basalo <albertobasalo@hotmail.com>
2020-03-05 19:57:41 +0100

    🏗 : layout components and styles

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShellComponent } from './shell/shell.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './shell/footer/footer.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { MainComponent } from './shell/main/main.component';
-import { FooterComponent } from './shell/footer/footer.component';
-
-
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule],
+  exports: [ShellComponent],
 })
-export class LayoutModule { }
+export class LayoutModule {}
